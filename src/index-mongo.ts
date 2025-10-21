c2ff0e66365213df8e086489d946297501d8415c
Guilherme Figueiredo Terenciani <guilhermeterenciani@gmail.com>
2025-10-21 12:25:06 -0300

    Commit

--- src/index-mongo.ts
+++ src/index-mongo.ts
@@ -3,8 +3,9 @@ import express from 'express';
 import rotasAutenticadas from './rotas/rotas-autenticadas.js';
 import rotasNaoAutenticadas from './rotas/rotas-nao-autenticadas.js';
 import Auth from './middleware/auth.js';
-
+import cors from 'cors'
 const app = express();
+app.use(cors())
 app.use(express.json());
 
 
